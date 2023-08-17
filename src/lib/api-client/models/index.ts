fb467bf0e2ed78ae3e60b3d225f9e407909f7715
Long <119793901+tran-hai-long@users.noreply.github.com>
2023-08-17 20:03:19 +0700

    Import schema and generate API client

---
+++ src/lib/api-client/models/index.ts
@@ -0,0 +1,24 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CustomUser';
+export * from './CustomUserAuth';
+export * from './CustomUserLogin';
+export * from './JWT';
+export * from './PaginatedCustomUserList';
+export * from './PaginatedSocialAccountList';
+export * from './PaginatedTaskList';
+export * from './PasswordChange';
+export * from './PatchedCustomUser';
+export * from './PatchedCustomUserAuth';
+export * from './PatchedTask';
+export * from './PriorityEnum';
+export * from './ProviderEnum';
+export * from './Register';
+export * from './ResendEmailVerification';
+export * from './RestAuthDetail';
+export * from './SocialAccount';
+export * from './SocialConnect';
+export * from './SocialLogin';
+export * from './StatusEnum';
+export * from './Task';
+export * from './VerifyEmail';
